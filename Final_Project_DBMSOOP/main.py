c32563b0e11b5153c8d00d0bee647a8f3fdf888b
nheil15 <129248535+nheil15@users.noreply.github.com>
2023-12-02 17:22:44 +0800

    main.py

--- Final_Project_DBMSOOP/main.py
+++ Final_Project_DBMSOOP/main.py
@@ -2,6 +2,18 @@ from website import create_app
 
 app = create_app()
 
+@app.route('/') 
+def login():
+    return render_template('login.html')
+
+@app.route('/login, method=['GET', 'POST'])
+def signup():
+    return render_template('sign_up.html')
+
+@app.route('/signup, method=['GET', 'POST'])
+def index(): 
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
