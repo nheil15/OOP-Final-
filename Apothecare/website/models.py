04c6af1675913ed80f39ac8ebc36b53cf121159f
immaximo <129141794+immaximo@users.noreply.github.com>
2023-12-08 11:16:19 +0800

    Update models.py

--- Apothecare/website/models.py
+++ Apothecare/website/models.py
@@ -2,12 +2,6 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 class User(db.Model, UserMixin):
     UserId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UserEmail = db.Column(db.String(30), unique=True, nullable=False)
@@ -20,7 +14,7 @@ class User(db.Model, UserMixin):
 
     @property
     def is_active(self):
-        # You can customize this logic based on your application's requirements
+  
         return True
     @property
     def email(self):
